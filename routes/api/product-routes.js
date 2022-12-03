c72db37b392ccb0dee7da0b5305b517d586b0b63
mattmald <Mat.maldonado012@gmail.com>
2022-12-02 18:00:35 -0600

    added product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,30 +1,41 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const {productData} = await Product.findAll()
+     res.status(200).json({productData});
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {id: req.params.id},
+  }).then((Product) => {
+    res.json(Product)
+  })
 });
 
 // create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
+router.post('/', async (req, res) => {
+    try {
+    const tagData = await Tag.create({
+       product_name: "Basketball",
       price: 200.00,
       stock: 3,
       tagIds: [1, 2, 3, 4]
-    }
-  */
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+  
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -45,7 +56,7 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
+
 
 // update product
 router.put('/:id', (req, res) => {
@@ -91,6 +102,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where:{id: req.params.id}
+  }) 
+  .then((productDelete) => {
+    res.json(productDelete);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 //finds all tags
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll()
      res.status(200).json(tagData);
